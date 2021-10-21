6573c2109aa0d17ed1d293fa6b0998a483ddd48b
George Eccleston <george6030809@student.gbmc.ac.uk>
2021-10-21 19:49:50 +0100

    completed

--- MapGameForm/Form1.Designer.cs
+++ MapGameForm/Form1.Designer.cs
@@ -36,13 +36,14 @@ namespace MapGameForm
             this.button4 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(272, 168);
+            this.button1.Location = new System.Drawing.Point(12, 170);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(240, 38);
             this.button1.TabIndex = 1;
@@ -52,45 +53,59 @@ namespace MapGameForm
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(272, 212);
+            this.button2.Location = new System.Drawing.Point(12, 214);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(240, 38);
             this.button2.TabIndex = 1;
             this.button2.Text = "button1";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(272, 256);
+            this.button3.Location = new System.Drawing.Point(12, 258);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(240, 38);
             this.button3.TabIndex = 1;
             this.button3.Text = "button1";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(272, 300);
+            this.button4.Location = new System.Drawing.Point(12, 302);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(240, 38);
             this.button4.TabIndex = 1;
             this.button4.Text = "button1";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(272, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 14);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(240, 150);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(96, 343);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 50);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0/0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(267, 396);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -102,6 +117,7 @@ namespace MapGameForm
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -112,6 +128,7 @@ namespace MapGameForm
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MapGameForm/Form1.cs
+++ MapGameForm/Form1.cs
@@ -11,11 +11,17 @@ using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace MapGameForm
 {
     public partial class Form1 : Form
     {
+        public int currentFlag, correctFlagLocation;
+        public static string filePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Resources");
+        public int incrementorCor = 0;
+        public int incrementorTotal = 0;
+        public string[] imageCodeArray = Directory.GetFiles(filePath, "*");
         public Form1()
         {
             InitializeComponent();
@@ -23,54 +29,122 @@ namespace MapGameForm
 
         public void Form1_Load(object sender, EventArgs e)
         {
-            Random r = new Random();
+            initiallize();
+        }
+        public void initiallize()
+        {
             Button[] btnArray = { button1, button2, button3, button4 };
+            for (int i = 0; i < 4; i++)
+            {
+                btnArray[i].BackColor = Color.White;
+            }
+            createQuestion();
+
+        }
+        public void createQuestion()
+        {
+            Button[] btnArray = { button1, button2, button3, button4 };
+            Random r = new Random();
             int temp;
-            int currentFlag,correctFlagLocation;
             int[] flag = new int[3];
-            string[] imageCodeArray;
-            string filePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Resources");
-            imageCodeArray = Directory.GetFiles(filePath, "*");
-                currentFlag = r.Next(0, 256);
-                temp = r.Next(0, 256);
-                while (temp == currentFlag)
+            currentFlag = r.Next(0, imageCodeArray.Length - 1);
+            temp = r.Next(0, imageCodeArray.Length - 1);
+            while (temp == currentFlag)
+            {
+                temp = r.Next(0, imageCodeArray.Length - 1);
+            }
+            flag[0] = temp;
+            temp = r.Next(0, imageCodeArray.Length - 1);
+            while (temp == currentFlag || temp == flag[0])
+            {
+                temp = r.Next(0, imageCodeArray.Length - 1);
+            }
+            flag[1] = temp;
+            temp = r.Next(0, imageCodeArray.Length - 1);
+            while (temp == currentFlag || temp == flag[0] || temp == flag[1])
+            {
+                temp = r.Next(0, imageCodeArray.Length - 1);
+            }
+            flag[2] = temp;
+            pictureBox1.ImageLocation = imageCodeArray[currentFlag];
+            temp = 0;
+            correctFlagLocation = r.Next(0, 4);
+            for (int i = 0; i < 4; i++)
+            {
+                if (correctFlagLocation == i)
                 {
-                    temp = r.Next(0, 256);
+                    btnArray[i].Text = imageCodeArray[currentFlag].Replace(filePath, "").Replace("\\", "").Replace(".png", "");
                 }
-                flag[0] = temp;
-                temp = r.Next(0, 256);
-                while (temp == currentFlag || temp == flag[0])
+                else
                 {
-                    temp = r.Next(0, 256);
+                    btnArray[i].Text = imageCodeArray[flag[temp]].Replace(filePath, "").Replace("\\", "").Replace(".png", "");
+                    temp++;
                 }
-                flag[1] = temp;
-                temp = r.Next(0, 256);
-                while (temp == currentFlag || temp == flag[0] || temp == flag[1])
-                {
-                    temp = r.Next(0, 256);
-                }
-                flag[2] = temp;
-                pictureBox1.ImageLocation = imageCodeArray[currentFlag];
-                temp = 0;
-                correctFlagLocation = r.Next(0, 4);
-                for (int i = 0; i < 4; i++)
-                {
-                    if (correctFlagLocation == i)
-                    {
-                        btnArray[i].Text = imageCodeArray[currentFlag].Replace(filePath, "").Replace("\\", "").Replace(".png", "");
-                    }
-                    else
-                    {
-                        btnArray[i].Text = imageCodeArray[flag[temp]].Replace(filePath, "").Replace("\\", "").Replace(".png", "");
-                        temp++;
-                    }
-                }
+            }
+        }
+
+        public void questionResult(int buttonNum,bool correct)
+        {
+            Button[] btnArray = { button1, button2, button3, button4 };
+
+            if(correct==true)
+            {
+                btnArray[buttonNum].BackColor = Color.Green;
+                incrementorCor++;
+                incrementorTotal++;
+                increment();
+            }
+            else
+            {
+                btnArray[buttonNum].BackColor = Color.Red;
+                btnArray[correctFlagLocation].BackColor = Color.Green;
+                incrementorTotal++;
+                increment();
+            }
+            Refresh();
+            Thread.Sleep(1000);
         }
         public void button1_Click(object sender, EventArgs e)
         {
-           int buttonInt = Array.IndexOf(btnArray, sender as Button); //this is an error, cannot refer to btnArray as it doesnt exist in this context. not sure how to get around this
-            
+            button_click(sender as Button);
+
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button_click(sender as Button);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button_click(sender as Button);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button_click(sender as Button);
+        }
+        public void button_click(object sender)
+        {
+            Button[] btnArray = { button1, button2, button3, button4 };
+            int buttonInt = Array.IndexOf(btnArray, sender as Button);
+            bool correct;
+            if (buttonInt == correctFlagLocation)
+            {
+                correct = true;
+            }
+            else
+            {
+                correct = false;
+            }
+            questionResult(buttonInt, correct);
+            initiallize();
+        }
+        public void increment()
+        {
+            label1.Text = incrementorCor + "/" + incrementorTotal;
+        }
+
     }
 
 }
